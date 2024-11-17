3b365330215afd6a5e8bffb15d3ad39d90d9396c
Rodwan26 <rdwanbnmwsy064@gmail.com>
2024-11-17 11:54:02 +0100

    Update HTML, CSS, and JS files

---
+++ Note.js
@@ -0,0 +1,71 @@
+let Notes = document.getElementById("Notes");
+let WriteNotebtnValue = document.getElementById("WriteNote");
+
+let notesTable = [];
+getStorege();
+displayNotes();
+function displayNotes() {
+  Notes.innerHTML = "";
+  let i = 0;
+  for (note of notesTable) {
+    let a = note.body;
+    let content = `
+      <div class="note" id="note">
+            <p>${note.body}</p>
+            <img src="Trash.png" alt="trash" onclick="deleteNote(${i})"  class="trash" />
+            <img src="iddit.png" alt="eddit" onclick="eddit('${a}',${i})" class="eddit" />
+          </div>
+     `;
+    i++;
+    Notes.innerHTML += content;
+  }
+  storageNotes();
+}
+WriteNotebtn.addEventListener("click", () => {
+  WriteNotebtnValue = document.getElementById("WriteNote").value;
+  let note = {
+    body: WriteNotebtnValue,
+  };
+  notesTable.push(note);
+
+  displayNotes();
+  document.getElementById("WriteNote").value = "";
+});
+
+function deleteNote(i) {
+  notesTable.splice(i, 1);
+  displayNotes();
+}
+function storageNotes() {
+  let NotesJson = JSON.stringify(notesTable);
+  localStorage.setItem("Notes", NotesJson);
+}
+
+function getStorege() {
+  let el = localStorage.getItem("Notes");
+  notesTable = JSON.parse(el);
+  if (notesTable == null) {
+    notesTable = [];
+  } else {
+    notesTable = notesTable;
+  }
+}
+function eddit(p, index) {
+  document.getElementById("WriteNote").value = p;
+  deleteNote(index);
+  displayNotes();
+  disableEditButtons();
+}
+function disableEditButtons() {
+  let editButtons = document.querySelectorAll(".eddit");
+  editButtons.forEach((eddit) => {
+    eddit.style.pointerEvents = "none";
+    eddit.style.cursor = "not-allowed";
+  });
+}
+/*
+WriteNotebtnValue = document.getElementById("WriteNote").value;
+WriteNotebtnValue = p;
+deleteNote();
+displayNotes(i);
+console.log("hello"); */
---
+++ README.md
@@ -0,0 +1,3 @@
+"# Notes-app" 
+"# Notes-app" 
+"# Notes-app" 
